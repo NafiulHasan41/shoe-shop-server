157c42c9c6941f43390674d9beb3a6e124771fcb
NafiulHasan41 <nhashik40@gmail.com>
2024-05-19 01:23:25 +0600

    Made post routes for user login data saved , made get all sheos data routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const formData = require('form-data');
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -21,6 +22,28 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser())
+
+
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    if (!token) return res.status(401).send({ message: 'unauthorized access' })
+    if (token) {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          console.log(err)
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+      
+  
+        req.user = decoded
+      
+        next()
+      })
+    }
+  }
+
 
 
 
@@ -44,8 +67,89 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const userCollection = client.db("shoeShop").collection("users");
+    const shoeCollection = client.db("shoeShop").collection("shoes");
+
+
+       // jwt generating
+       app.post('/jwt', async (req, res) => {
+        const email = req.body
+     
+        console.log("email jwt" , email)
+        const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '2h',
+        })
+        res
+          .cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+      })
+
+
+        // Clear  the token when logout
+        app.get('/logout', (req, res) => {
+
+            res
+              .clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                maxAge: 0,
+              })
+              .send({ success: true })
+          })
+
+          //user operation
+          app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user doesnt exists: 
+            // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+              return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            console.log('result', result)
+            res.send(result);
+          });
+
+
+   
+   
+          //shoe section
+          app.get('/shoes', async (req, res) => {
+            const size = parseInt(req.query.size)
+            const page = parseInt(req.query.page) - 1
+            const filter = req.query.filter
+            const sort = req.query.sort
+            const search = req.query.search
+            const shoeSize = req.query.shoeSize
+            
+   
+      
+            let query = {
+                title: { $regex:'^' + search, $options: 'i' },
+            }
+            if (filter) query.category = filter
+            if (shoeSize) query.size = shoeSize
+            let options = {}
+            if (sort) options = { sort: { price: sort === 'asc' ? 1 : -1 } }
+            const result = await shoeCollection.find(query, options).skip(page * size).limit(size).toArray()
+
+             
+
+      
+            res.send(result)
+          })
+      
     
-    
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
